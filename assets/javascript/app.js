5cf318e4ad5610c057571e3d25d3e2ec932e8adf
Peter-Li1233 <minxiangli08@gmail.com>
2018-06-13 15:32:55 -0400

    first version psudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+//Define an object for each question, including question/choices/method (return the correct answer)
+//Define an array to store all the object;
+//Define an winCount;
+//Define an LossCount;
+//Define an variable to store the unanswered;
+
+//play
+    //for loopt hrough each item of the array;{}
+        //Set up an timer and display it on the screen;
+        // put the question and choices on the screen;
+            //create an button for each choice
+
+        //if the timers is up and no choice made {;
+            // show the time is up;
+            // show the right answer;
+            // move onto next question;
+            // setup timer and display it on the screen;
+            //Put the question and choices on the screen;
+        //} else if the choice is made within the time {
+            // decide whether the choice is correct; (clicked html = object.correctmethod);
+            //if the choice is correct {
+                //stop the timer;
+                //display it let the user know it's correct
+                //play an celebration video;
+                //Go to next question
+                //setup timer and display
+                //put the question on the screen
+            //} else if {
+                //stop the timer;
+                //display it let the user know it's wrong;
+                // show the correct answer;
+                //play an unfortunate video
+                //go to next question
+                //.....
+                //.....
+             //}
+
+        //}
+//}
+
+// display the winCount;
+// display the lossCount
+// Create an "start over" button
+// if the user clicked, play again (called the play function)
