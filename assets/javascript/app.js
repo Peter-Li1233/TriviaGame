2a49d9cb92fe10d96a5baefc1b061aba1c10c49b
Peter-Li1233 <minxiangli08@gmail.com>
2018-06-19 13:54:03 -0400

    made code more easy to read

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -123,78 +123,104 @@ window.onload = function() {
 
   };
   
-  var trivia = {
-    //Define an object for each question, including question/choices/method (return the correct answer)
-    triaviaLibrary: [ 
-    {question: "what is the only bird known to fly backwards ?", 
-    a: "Hummingbird", 
-    b: "sparrow", 
-    c: "Blue Jay", 
-    d: "Mourning Dove", 
-    correctAnswer: function(){return this.a}
-    },
+    var trivia = {
+      //Define an object for each question, including question/choices/method (return the correct answer)
+      //Define an array to store all the object;
+      triaviaLibrary: [ 
+        {question: "what is the only bird known to fly backwards ?", 
+        a: "Hummingbird", 
+        b: "sparrow", 
+        c: "Blue Jay", 
+        d: "Mourning Dove", 
+        correctAnswer: function(){return this.a}
+        },
 
-    {question: "In China, what number is considered unlucky because its pronunciation is similar to that for the word 'death' ?", 
-    a: "8", 
-    b: "7", 
-    c: "4", 
-    d: "10", 
-    correctAnswer: function(){return this.c}
-    },
+        {question: "In China, what number is considered unlucky because its pronunciation is similar to that for the word 'death' ?", 
+        a: "8", 
+        b: "7", 
+        c: "4", 
+        d: "10", 
+        correctAnswer: function(){return this.c}
+        },
 
-    {question: "The Martial art of Kung Fu originated in which conuntry ?", 
-    a: "India", 
-    b: "Korea", 
-    c: "Japan", 
-    d: "China", 
-    correctAnswer: function(){return this.d}
-    },
-    {question: "What NBA player was known as 'The Pearl' ?", 
-    a: "James Harden", 
-    b: "Earl Monroe", 
-    c: "Kobe Brant", 
-    d: "Michael Jordan", 
-    correctAnswer: function(){return this.b}
-    },
+        {question: "The Martial art of Kung Fu originated in which conuntry ?", 
+        a: "India", 
+        b: "Korea", 
+        c: "Japan", 
+        d: "China", 
+        correctAnswer: function(){return this.d}
+        },
+        {question: "What NBA player was known as 'The Pearl' ?", 
+        a: "James Harden", 
+        b: "Earl Monroe", 
+        c: "Kobe Brant", 
+        d: "Michael Jordan", 
+        correctAnswer: function(){return this.b}
+        },
 
-    {question: "the world's fastest growing plant is a species of what ?", 
-    a: "Flowering Plants", 
-    b: "Mosses", 
-    c: "Grass", 
-    d: "Bamboo", 
-    correctAnswer: function(){return this.d}
-    },
+        {question: "the world's fastest growing plant is a species of what ?", 
+        a: "Flowering Plants", 
+        b: "Mosses", 
+        c: "Grass", 
+        d: "Bamboo", 
+        correctAnswer: function(){return this.d}
+        },
 
-    {question: "On the Apollo 11 moon mission, which astronaut stayed aloft in the command module while Neil Armstrong and Buzz Aldrin walked on the moon ?", 
-    a: "Peter Li", 
-    b: "Donald Trump", 
-    c: "Michael Collins", 
-    d: "Hillary Clinton", 
-    correctAnswer: function(){return this.c}
-    },
+        {question: "On the Apollo 11 moon mission, which astronaut stayed aloft in the command module while Neil Armstrong and Buzz Aldrin walked on the moon ?", 
+        a: "Peter Li", 
+        b: "Donald Trump", 
+        c: "Michael Collins", 
+        d: "Hillary Clinton", 
+        correctAnswer: function(){return this.c}
+        },
 
-    {question: "When a drink is served 'on the rocks', it is served with what ?", 
-    a: "Ice Cubes", 
-    b: "Creamy Cheese", 
-    c: "Salt", 
-    d: "Sugar", 
-    correctAnswer: function(){return this.a}
-    }
-  ], 
+        {question: "When a drink is served 'on the rocks', it is served with what ?", 
+        a: "Ice Cubes", 
+        b: "Creamy Cheese", 
+        c: "Salt", 
+        d: "Sugar", 
+        correctAnswer: function(){return this.a}
+        }
+      ], 
 
-    //Define an array to store all the object;
-    //triaviaLibrary: [this.triavia_1,this.triavia_2,this.triavia_3,this.triavia_4,this.triavia_5,this.triavia_6,this.triavia_7],
-  
-    //Define an winCount;
-    winCount:0,
-    //Define an LossCount;
-    lossCount:0,
-    //Define an variable to store the unanswered;
-    unAnswered:0,
-    //Define an array to store the index of answered question
-    libraryIndex:[],
+        
+      //Define an winCount;
+      winCount:0,
+      //Define an LossCount;
+      lossCount:0,
+      //Define an variable to store the unanswered;
+      unAnswered:0,
+      //Define an array to store the index of answered question
+      libraryIndex:[],
+ 
+      //Define an function to initialize for each round of play;
+      initialize: function() {
+          
+          var gameHeader = $('<div class = "game_header">');
+              gameHeader.text("Total Trival Trivia!")
 
-    showTrivia: function(o) {
+          var startTimer = $('<div class = "timer">');
+          var answerDiv = $('<div class = "answer">');
+
+          var startButton =$('<button class = "start">');
+              startButton.text("start");
+          
+          var subContainer = $('<div class = "sub_container">');
+          
+          $(".container").empty();
+          $(".container").append(gameHeader, startTimer, answerDiv,startButton, subContainer);
+
+          trivia.winCount = 0;
+          //Define an LossCount;
+          trivia.lossCount = 0;
+          //Define an variable to store the unanswered;
+          trivia.unAnswered = 0;
+          //Define an array to store the index of answered question
+          trivia.libraryIndex = [];
+
+      },
+      //Define an function to show up the trivia question in Window;
+      showTrivia: function(o) {
           var question = o.question;
           var a = o.a;
           var b = o.b;
@@ -210,185 +236,145 @@ window.onload = function() {
           $(".choices").append("<button class = 'button button2' value = 'b'>"+ b +"</button");
           $(".choices").append("<button class = 'button button3' value = 'c'>"+ c +"</button");
           $(".choices").append("<button class = 'button button4' value = 'd'>"+ d +"</button");
+        
+      },
       
-          },
-      
+      //Define an function to show the answer for each question and play a gif for correct and Incorrect answer;
       showAnswer: function(o,buttonClicked) {
-        var correctAnswer =o.correctAnswer();
-            console.log(o);
-            console.log(correctAnswer);
+          var correctAnswer =o.correctAnswer();
+              console.log(o);
+              console.log(correctAnswer);
 
-        var buttonContent = buttonClicked;
-            console.log(buttonContent);
-        var celebration = $('<img class = "celebration">');
+          var buttonContent = buttonClicked;
+              console.log(buttonContent);
+          var celebration = $('<img class = "celebration">');
+              console.log( $(".celebration"));
+          var disappointing = $('<img class = "disappointing">'); 
+        
+          if (buttonContent === correctAnswer) {
+            console.log("Yes");
+            trivia.winCount++;
+            $(".answer").html("<h4>Correct!</h4>");
+            $(".sub_container").empty();
+            $(".sub_container").append(celebration);
             console.log( $(".celebration"));
-        var disappointing = $('<img class = "disappointing">'); 
-       
-        if (buttonContent === correctAnswer) {
-          console.log("Yes");
-          trivia.winCount++;
-          $(".answer").html("<h4>Correct!</h4>");
-          $(".sub_container").empty();
-          $(".sub_container").append(celebration);
-          console.log( $(".celebration"));
-          $(".celebration").attr("src", "assets/gifs/celebration.gif");
-          $(".celebration").css("width", "300px");
-          $(".celebration").css("height","180px");
-          $(".celebration").css("align-self","center");
-          console.log( $(".celebration"));
+            $(".celebration").attr("src", "assets/gifs/celebration.gif");
+            $(".celebration").css("width", "300px");
+            $(".celebration").css("height","180px");
+            $(".celebration").css("align-self","center");
+            console.log( $(".celebration"));
 
-        } else {
-          console.log("Nope");
-          trivia.lossCount++;
-          $(".answer").html("<h4>Nope! the correct answer is: " + correctAnswer +"</h4>");
-          $(".sub_container").empty();
-          $(".sub_container").append(disappointing);
-          $(".disappointing").attr("src", "assets/gifs/disappointing.gif");
-          $(".disappointing").css("width", "300px");
-          $(".disappointing").css("height","180px");
-          $(".disappointing").css("align-self","center");
-        }
+          } else {
+            console.log("Nope");
+            trivia.lossCount++;
+            $(".answer").html("<h4>Nope! the correct answer is: " + correctAnswer +"</h4>");
+            $(".sub_container").empty();
+            $(".sub_container").append(disappointing);
+            $(".disappointing").attr("src", "assets/gifs/disappointing.gif");
+            $(".disappointing").css("width", "300px");
+            $(".disappointing").css("height","180px");
+            $(".disappointing").css("align-self","center");
+          }
 
 
       },
 
+        //Define an function to show the final result and create an button remainding "play again";
       showFinals: function() {
-        $(".container").empty()
-        $(".container").append("<p class ='summary'>ALL Done, Heres how you did! </p>");
-        $(".container").append("<p class ='summary'>" +"Correct Answer: " + trivia.winCount + "</p>");
-        $(".container").append("<p class ='summary'>" +" incorrect Answer: " + trivia.lossCount + "</p>");
-        $(".container").append("<p class ='summary'>" +"unAnswered: " + trivia.unAnswered + "</p>");
-        $(".container").append("<button class ='playAgain'>Play Again?</button>");
+          $(".container").empty()
+          $(".container").append("<p class ='summary'>ALL Done, Heres how you did! </p>");
+          $(".container").append("<p class ='summary'>" +"Correct Answer: " + trivia.winCount + "</p>");
+          $(".container").append("<p class ='summary'>" +" incorrect Answer: " + trivia.lossCount + "</p>");
+          $(".container").append("<p class ='summary'>" +"unAnswered: " + trivia.unAnswered + "</p>");
+          $(".container").append("<button class ='playAgain'>Play Again?</button>");
 
       },
 
-      initialize: function() {
-        
-        var gameHeader = $('<div class = "game_header">');
-            gameHeader.text("Total Trival Trivia!")
+      //Define an function to generate an random number so I can play trivia in an random(but no repeat) order
+        //instead of fixed order; 
+        generateRandom: function() {
+          var randomNum = Math.floor(Math.random()*7);
 
-        var startTimer = $('<div class = "timer">');
-        var answerDiv = $('<div class = "answer">');
-
-        var startButton =$('<button class = "start">');
-            startButton.text("start");
-        
-        var subContainer = $('<div class = "sub_container">');
-        
-        $(".container").empty();
-        $(".container").append(gameHeader, startTimer, answerDiv,startButton, subContainer);
-
-        trivia.winCount = 0;
-        //Define an LossCount;
-        trivia.lossCount = 0;
-        //Define an variable to store the unanswered;
-        trivia.unAnswered = 0;
-        //Define an array to store the index of answered question
-        trivia.libraryIndex = [];
-
-      },
+              while(trivia.libraryIndex.includes(randomNum)) {
+                    randomNum = Math.floor(Math.random()*7);
+                  }
+              trivia.libraryIndex.push(randomNum);
+              console.log(trivia.libraryIndex);
+              return randomNum;
+            
+        },  
 
       play: function() {
-        var i = trivia.generateRandom();
-        var timeoutID;
-        var triaviaObject;
+          var i = trivia.generateRandom();
+          var timeoutID;
+          var triaviaObject;
 
-        //if (trivia.triaviaLibrary.length < 7)  {
-        
-        triaviaObject = trivia.triaviaLibrary[i];
-
-        $(".answer").empty();
-        $(".sub_container").empty();
-        var question = $('<div class = "question">');
-        var choices = $('<div class = "choices">');
-
-        $(".sub_container").append(question,choices);
-
-        trivia.showTrivia(triaviaObject);
-        timer.reset();
-        timer.start();
-
-        $(".button").click(function(){
-          console.log(this);
-          timer.stop();
-          var buttonContent = $(this).text();
-          console.log (buttonContent);
-          trivia.showAnswer(triaviaObject, buttonContent);
-          // console.log(trivia.t)
-          // debugger;
-          if (trivia.libraryIndex.length >= 7)  {
-            clearTimeout(timeoutID);
-            setTimeout((trivia.showFinals), 3000);
-          } else {
-          setTimeout((trivia.play), 3000);
-          clearTimeout(timeoutID);    }                                    
-        });
-        
-        //if (trivia.libraryIndex.length >= 7)  {
-         // trivia.showFinals();
-        //} else {
-        timeoutID = setTimeout(showTimeup, 30000); 
-        //} 
-
-     // } else {trivia.showFinals()}
-
-        function showTimeup() {
-          var correctAnswer =triaviaObject.correctAnswer();
-          trivia.unAnswered++;
-          $(".answer").html("<h4>Time is up! the correct answer is: " + correctAnswer +"</h4>");
-  
-          $(".sub_container").empty();
-          var disappointing = $('<img class = "disappointing">'); 
-          $(".sub_container").append(disappointing);
-          $(".disappointing").attr("src", "assets/gifs/disappointing.gif");
-          $(".disappointing").css("width", "300px");
-          $(".disappointing").css("height","180px");
-          $(".disappointing").css("align-self","center");
-  
-          //setTimeout(trivia.play, 3000);
-
-          if (trivia.libraryIndex.length >= 7)  {
-            
-            setTimeout((trivia.showFinals), 3000);
-          } else {
-            setTimeout((trivia.play), 3000);
-          }    
-  
-        }
+          //if (trivia.triaviaLibrary.length < 7)  {
           
-      },
+          triaviaObject = trivia.triaviaLibrary[i];
 
-      showTimeup: function() {
-        //var correctAnswer =o.correctAnswer();
-        //$(".answer").html("<h4>Time is up! the correct answer is: " + correctAnswer +"</h4>");
-        trivia.unAnswered++;
-        $(".sub_container").empty();
-        var disappointing = $('<img class = "disappointing">'); 
-        $(".sub_container").append(disappointing);
-        $(".disappointing").attr("src", "assets/gifs/disappointing.gif");
-        $(".disappointing").css("width", "300px");
-        $(".disappointing").css("height","180px");
-        $(".disappointing").css("align-self","center");
+          $(".answer").empty();
+          $(".sub_container").empty();
+          var question = $('<div class = "question">');
+          var choices = $('<div class = "choices">');
 
-        setTimeout(trivia.play, 3000);
+          $(".sub_container").append(question,choices);
 
-      },
-       
+          trivia.showTrivia(triaviaObject);
+          timer.reset();
+          timer.start();
 
+          $(".button").click(function(){
+            console.log(this);
+            timer.stop();
+            var buttonContent = $(this).text();
+            console.log (buttonContent);
+            trivia.showAnswer(triaviaObject, buttonContent);
+            // console.log(trivia.t)
+            // debugger;
+            if (trivia.libraryIndex.length >= 7)  {
+              clearTimeout(timeoutID);
+              setTimeout((trivia.showFinals), 3000);
+            } else {
+            setTimeout((trivia.play), 3000);
+            clearTimeout(timeoutID);    }                                    
+          });
+          
+          //if (trivia.libraryIndex.length >= 7)  {
+          // trivia.showFinals();
+          //} else {
+          timeoutID = setTimeout(showTimeup, 30000); 
+          //} 
+
+      // } else {trivia.showFinals()}
+
+          function showTimeup() {
+              var correctAnswer =triaviaObject.correctAnswer();
+              trivia.unAnswered++;
+              $(".answer").html("<h4>Time is up! the correct answer is: " + correctAnswer +"</h4>");
       
+              $(".sub_container").empty();
+              var disappointing = $('<img class = "disappointing">'); 
+              $(".sub_container").append(disappointing);
+              $(".disappointing").attr("src", "assets/gifs/disappointing.gif");
+              $(".disappointing").css("width", "300px");
+              $(".disappointing").css("height","180px");
+              $(".disappointing").css("align-self","center");
+      
+              //setTimeout(trivia.play, 3000);
 
-      generateRandom: function() {
-        var randomNum = Math.floor(Math.random()*7);
-
-            while(trivia.libraryIndex.includes(randomNum)) {
-                  randomNum = Math.floor(Math.random()*7);
-                }
-            trivia.libraryIndex.push(randomNum);
-            console.log(trivia.libraryIndex);
-            return randomNum;
-           
-      }
-    };
+              if (trivia.libraryIndex.length >= 7)  {
+                
+                setTimeout((trivia.showFinals), 3000);
+              } else {
+                setTimeout((trivia.play), 3000);
+              }    
+          }
+            
+        },
+        
+        
+      };
 
   var timer = {
 
